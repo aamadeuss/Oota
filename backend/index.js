d87d5085a1a3d008af3db2fab5f69d3b26d1ed53
aamadeuss <ayushmaan017+github@gmail.com>
2023-12-10 19:40:16 +0530

    test commit #2

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ global.foodData = require('./db')(function call(err, data, MealData) {
   global.foodData = data;
   global.mealName = MealData;
 })
-
+//testing
 const environment = process.env.NODE_ENV || 'development';
 const configPath = './env-local.yaml';
 const configFile = fs.readFileSync(configPath, 'utf8');
