dc283f42c1610510469265e225b085f3a806224d
aamadeuss <ayushmaan017+github@gmail.com>
2023-12-10 18:11:39 +0530

    added product route testing

--- backend/productRoute.test.js
+++ backend/productRoute.test.js
@@ -30,5 +30,34 @@ describe('Review routes', () => {
         expect(response.body[1]).toBeDefined();
     });
 
-    
+    it('should store first review from new user', async () => {
+        const email = 'a@ba.com';
+        const orderData = [{ product: 'Product 1', quantity: 2 }, { product: 'Product 2', quantity: 1 }];
+        const orderDate = new Date();
+        const response = await request(server)
+        .post('/api/auth/orderData')
+        .send({email, order_data: orderData, order_date: orderDate});
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+    });
+
+    it('should store review from existing user', async () => {
+        const email = 'a@a.com';
+        const orderData = [{ product: 'Product 1', quantity: 2 }, { product: 'Product 2', quantity: 1 }];
+        const orderDate = new Date();
+        const response = await request(server)
+        .post('/api/auth/orderData')
+        .send({email, order_data: orderData, order_date: orderDate});
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+    });
+
+    it('should get reviews from email', async () => {
+        const email = 'a@a.com';
+        const response = await request(server)
+        .post('/api/auth/myOrderData')
+        .send({email});
+        expect(response.status).toBe(200);
+        expect(response.body.orderData).toBeDefined();
+    });
 });
